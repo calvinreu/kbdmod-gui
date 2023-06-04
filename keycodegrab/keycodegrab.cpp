0e060125c2894b6819b7ca76970bfe6e1437e57d
calvinreu <j.calvin.reu@protonmail.com>
2023-06-04 08:28:27 +0200

    redo the work since this is dogshit

--- keycodegrab/keycodegrab.cpp
+++ keycodegrab/keycodegrab.cpp
@@ -1,9 +1,6 @@
-//include input event header file
 #include <linux/input-event-codes.h>
 #include <linux/input.h>
-//include header file setbuf
 #include <stdio.h>
-//include header file atoi
 #include <stdlib.h>
 #include <fstream>
 #include <iostream>
@@ -11,38 +8,39 @@
 int main(int argc, char *argv[]) {
     input_event ev;
     if (argc != 3) {
-        std::cout << "Usage: keycodegrab <number of keys to log> <output file name>" << std::endl;
+        std::cout << 
+'Usage: keycodegrab <output file name>\nto quit press 1 and 0 at the same time'
+        << std::endl;
         return 1;
     }
-    //get the number of keys to log
-    int numKeys = atoi(argv[1]);
-    //get the output file name
     std::string outputFileName = argv[2];
 
-    //create output file
     std::ofstream outputFile;
     outputFile.open(outputFileName);
-    //check if output file is open
     if(!outputFile.is_open()) {
         std::cout << "Error opening output file" << std::endl;
         return 1;
     }
-    //clear output file
     outputFile.clear();
+    u_int16_t prevcode=0;
 
-    //clear input buffer
     setbuf(stdin, NULL);
-    //loop till new event is detected
-    for(int i = 0; i < numKeys ; i++) {
-        //read input event
+    setbuf(stdout, NULL);
+    
+    while(true) {
         if(fread(&ev, sizeof(input_event), 1, stdin) != 1) {
             std::cout << "Error reading input event" << std::endl;
             return -1;
         }
-        //check if event is key press
+        fwrite(&ev, sizeof(input_event), 1, stdout);
         if(ev.type == EV_KEY && ev.value == 1) {
-            //write key code to output file
             outputFile << ev.code << std::endl;
+            //quit if 1 and 0 are pressed at the same time
+            if(ev.code == KEY_1 && prevcode == KEY_0 ||
+               ev.code == KEY_0 && prevcode == KEY_1) {
+                break;
+            }
+            prevcode = ev.code;
         }
     }
 
